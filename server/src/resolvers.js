d13ab8c899e38f731f78717515e8f5491c320a55
Steven <hai_gong@swissre.com>
2021-04-13 10:20:18 +1000

    connecting-the-dots-in-server-land

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,8 +1,16 @@
+// src/resolvers.js
 const resolvers = {
     Query: {
-        // returns an array of Tracks that will be used to populate
-        // the homepage grid of our web client
-        tracksForHome: (parent, args, context, info) => { },
+        // get all tracks, will be used to populate the homepage grid of our web client
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
     }
 };
+
 module.exports = resolvers;
